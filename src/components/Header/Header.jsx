194f74bfbbea4a4734ab5debecb7950659d102a5
parkjihun22 <eyeful31@naver.com>
2025-02-17 11:33:53 +0900

    feat : 헤더수정

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -104,9 +104,9 @@ const Header = () => {
         <div className={styles.mobileHeader}>
           <div onClick={() => setIsMobileMenu(!isMobileMenu)}>
             {!isMobileMenu ? (
-              <AiOutlineMenu className={styles.icon} size={25} color="#095052" />
+              <AiOutlineMenu className={styles.icon} size={25} color="#2d3c49" />
             ) : (
-              <IoCloseSharp className={styles.icon} size={25} color="#095052" />
+              <IoCloseSharp className={styles.icon} size={25} color="#2d3c49" />
             )}
           </div>
           {isMobileMenu && (
@@ -122,7 +122,7 @@ const Header = () => {
           </Link>
           {/* 모바일에서는 기존 전화 연결 a 태그 유지 */}
           <a href="tel:1533-8848">
-            <IoCall className={styles.icon} size={25} color="#095052" />
+            <IoCall className={styles.icon} size={25} color="#2d3c49" />
           </a>
         </div>
         {/* 모바일에서도 팝업을 렌더링 (최상위에서) */}
