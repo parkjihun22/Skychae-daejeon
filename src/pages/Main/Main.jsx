b35aeaf816e850319630e7b913a786a97e47c2b6
parkjihun22 <eyeful31@naver.com>
2025-03-11 10:23:52 +0900

    feat : 백링크해제

--- src/pages/Main/Main.jsx
+++ src/pages/Main/Main.jsx
@@ -13,7 +13,7 @@ import UnitplanBox from "../../components/UnitplanBox/UnitplanBox";
 import MobilePopup from "../../components/MobilePopup/MobilePopup";
 import Popup from "../../components/Popup/Popup";
 import MobileSectionBox from "../../components/MobileSectionBox/MobileSectionBox";
-import UrlContainer from "../../components/UrlContainer/UrlContainer";
+// import UrlContainer from "../../components/UrlContainer/UrlContainer";
 
 
 import mainImage from "../../assets/Main/Main1.jpg";
@@ -427,7 +427,7 @@ const Main = () => {
           </div> */}
 
           <div className={styles.section5}>
-            <UrlContainer />
+
             <Footer />
           </div>
           {/* 방문예약 팝업 (PC) */}
@@ -643,7 +643,6 @@ const Main = () => {
           </div> */}
 
           <div className={styles.section5}>
-            <UrlContainer />
             <Footer />
             <FixIcon />
           </div>
